ea3ccf49402d88db08302a8bddafd2fe3de58d16
Soren <130689083+GoChipEcho@users.noreply.github.com>
2023-12-16 14:22:49 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,47 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import store from "./reducers";
 import { Provider } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Battery from "../../components/shared/Battery";
+import { Icon, Image } from "../../utils/general";
+import "./back.scss";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <Suspense
-    fallback={
-      <div id="sus-fallback">
-        <h1>Loading</h1>
-      </div>
-    }
-  >
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Suspense>,
-);
+
+export const Background = () => {
+  const wall = useSelector((state) => state.wallpaper);
+  const dispatch = useDispatch();
+
+  return (
+    <div
+      className="background"
+      style={{
+        backgroundImage: `url(img/wallpaper/${wall.src})`,
+      }}
+    ></div>
+  );
+};
+
+
+<Image
+          className="rounded-full overflow-hidden"
+          src="img/asset/prof.jpg"
+          w={200}
+          ext
+        />
+        <div className="mt-2 text-2xl font-medium text-gray-200">
+          {userName}
+        </div>
+
+<Image src="asset/bootlogo" w={180} />
+        <div className="mt-48" id="loader">
+          <svg
+            className="progressRing"
+            height={48}
+            width={48}
+            viewBox="0 0 16 16"
+          >
+            <circle cx="8px" cy="8px" r="7px"></circle>
+          </svg>
+        </div>
