08c83cdcf7a7f1a3ceecf4ca71ab7ed8283fdf98
Soren <130689083+GoChipEcho@users.noreply.github.com>
2023-12-16 18:02:58 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,47 +3,82 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import store from "./reducers";
 import { Provider } from "react-redux";
-import { useDispatch, useSelector } from "react-redux";
-import Battery from "../../components/shared/Battery";
-import { Icon, Image } from "../../utils/general";
-import "./back.scss";
 
 const root = createRoot(document.getElementById("root"));
 
+root.render(
+  <Suspense
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Poppins&family=Roboto&family=Space+Mono&family=Work+Sans&display=swap" rel="stylesheet">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
 
-export const Background = () => {
-  const wall = useSelector((state) => state.wallpaper);
-  const dispatch = useDispatch();
+.container {
+    position: relative;
+    text-align: center;
+    color: white;
+}
+  
+.welcome{
+  position: absolute;
+  top: 70%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: 'Poppins', sans-serif;
+  font-size: 25;
+  color: white;
+}
 
-  return (
-    <div
-      className="background"
-      style={{
-        backgroundImage: `url(img/wallpaper/${wall.src})`,
-      }}
-    ></div>
-  );
-};
+.bg {
+  background-image: url("/src/loadingWindows.jpg");
 
+  /* Full height */
+  height: 100%; 
 
-<Image
-          className="rounded-full overflow-hidden"
-          src="img/asset/prof.jpg"
-          w={200}
-          ext
-        />
-        <div className="mt-2 text-2xl font-medium text-gray-200">
-          {userName}
-        </div>
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 
-<Image src="asset/bootlogo" w={180} />
-        <div className="mt-48" id="loader">
-          <svg
-            className="progressRing"
-            height={48}
-            width={48}
-            viewBox="0 0 16 16"
-          >
-            <circle cx="8px" cy="8px" r="7px"></circle>
-          </svg>
-        </div>
+  .loader {
+    border: 4px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 4px solid #3498db;
+    width: 15px;
+    height: 15px;
+    -webkit-animation: spin 2s linear infinite; /* Safari */
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+</style>
+<div class="startupWelcome">
+<div class="bg"></div>
+<center class="welcome">
+  Welcome 
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <div class="loader"></div>
+</center>
+</div>
+    fallback={
+      <div id="sus-fallback">
+      </div>
+    }
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Suspense>,
+);
